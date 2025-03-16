4f072af7233c922b71dacb89d226b0193183127a
antonialeach <antonia.leach@student.upt.ro>
2025-03-17 01:44:48 +0200

    Generated QR code

--- main.go
+++ main.go
@@ -6,9 +6,11 @@ import (
 	"time"
 
 	"github.com/pquerna/otp/totp"
+	"github.com/skip2/go-qrcode"
 )
 
 func main() {
+	//Gerating secret
 	secret, err := totp.Generate(totp.GenerateOpts{
 		Issuer:      "My App",
 		AccountName: "user@example.com",
@@ -18,6 +20,19 @@ func main() {
 	}
 	fmt.Println("Secret: ", secret.Secret())
 
+	//Generating QR code
+	qrCode, err := qrcode.Encode(secret.URL(), qrcode.Medium, 256)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = qrcode.WriteFile(secret.URL(), qrcode.Medium, 256, "qrcode.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(qrCode))
+	fmt.Println("QR Code saved successfully in qrcode.png")
+
+	//Gerating TOTP code
 	code, err := totp.GenerateCode(secret.Secret(), time.Now())
 	if err != nil {
 		log.Fatal(err)
